279e4b4c85db826fa7bc1b6f791b42e36dee8a41
ShauryaShsin <singhshaurya.16@gmail.com>
2023-09-21 12:48:14 +0200

    First commit to betting-algorithm

---
+++ data_cleaning.r
@@ -0,0 +1,8 @@
+###########################################
+# Author: Shaurya Singh
+# Topic: EPL Sports Betting Algorithm
+# Course: Empirical Economics
+# Term: 2022 Winter
+###########################################
+
+# Load Data
